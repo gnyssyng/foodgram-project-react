9d6d1e4b96f4b8e6552c3caf2cfdc59c0f1ee164
gnyssyng <hdneal@yandex.ru>
2024-04-30 19:43:48 +0300

    We did it, boys(?)

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,27 +2,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
-from django.views.generic import TemplateView
-from rest_framework.permissions import AllowAny
-from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(
-        'openapi',
-        get_schema_view(
-            permission_classes=[AllowAny],
-        ),
-        name="openapi-schema",
-    ),
-    path(
-        'api/docs/',
-        TemplateView.as_view(
-            template_name='redoc.html',
-            extra_context={'schema_url': 'openapi-schema'}
-        ),
-        name='redoc'
-    ),
     path('api/', include('api.urls'))
 ]
 if settings.DEBUG is True:
