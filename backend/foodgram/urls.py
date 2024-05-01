015e7085b2b7e9bf104817c1fda061ee5c1b0c7d
gnyssyng <hdneal@yandex.ru>
2024-05-01 14:46:38 +0300

    a

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,10 +2,28 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
+from rest_framework.permissions import AllowAny
+from rest_framework.schemas import get_schema_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path(
+        'openapi',
+        get_schema_view(
+            permission_classes=[AllowAny],
+        ),
+        name="openapi-schema",
+    ),
+    path(
+        'api/docs/',
+        TemplateView.as_view(
+            template_name='redoc.html',
+            extra_context={'schema_url': 'openapi-schema'}
+        ),
+        name='redoc'
+    ),
 ]
 if settings.DEBUG is True:
     urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
